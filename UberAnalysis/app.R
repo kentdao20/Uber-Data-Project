72eed8c7ff52a521574c569dacd8b9cac4cd1931
kienkcp <kentdaokcp@gmail.com>
2023-04-25 15:13:52 -0500

    Shiny

--- UberAnalysis/app.R
+++ UberAnalysis/app.R
@@ -8,44 +8,120 @@
 #
 
 library(shiny)
+library(DT)
+library(tidyverse)
+library(rsconnect)
+library(ggplot2)
+
+getwd()
+
+
+day_group <- read.csv("day_group.csv")
+day_month_group <- read.csv("day_month_group.csv")
+hour_data <- read.csv("hour_data.csv")
+month_group <- read.csv("month_group.csv")
+month_hour <- read.csv("month_hour.csv")
+month_weekday <- read.csv("month_weekday.csv")
+prediction_model <- read.csv("prediction_model.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  titlePanel("Uber Data"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("select", "Select Data", choices = c("day_group", "day_month_group", "hour_data", "month_group", "month_hour", "month_weekday", "prediction_model"), selected = "day_group")
+    ),
+    mainPanel(
+      plotOutput("bar_chart"),
+      DT::dataTableOutput("table")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+server <- function(input, output)
+{
+  
+  
+  # Reactive data based on selected data
+  selected_data <- reactive({
+    switch(input$data_select,
+           "day_group" = day_group,
+           "day_month_group" = day_month_group,
+           "hour_data" = hour_data,
+           "month_group" = month_group,
+           "month_hour" = month_hour,
+           "month_weekday" = month_weekday,
+           "prediction_model" = prediction_model)
+  })
+  
+  # Render bar chart
+  output$bar_chart <- renderPlot({
+    #using if and else statement as per users selection
+    #if the user selects base and day data then it display x and y depending on the table value
+    if (input$data_select == "day_group`  ") {
+      ggplot(selected_data(), aes(x = dayofweek, y = Total, fill = Base)) +
+        geom_bar(stat = "identity") +
+        labs(title = "Interactive Bar Chart", x = "Day of Week", y = "Total") +
+        theme_minimal()
+    }
+    #if the user selects base and month data then it display x and y depending on the table value
+    else if (input$data_select == "df_base_month") {
+      ggplot(selected_data(), aes(x = Month, y = Total, fill = Base)) +
+        geom_bar(stat = "identity") +
+        labs(title = "Interactive Bar Chart", x = "Month", y = "Total") +
+        theme_minimal()
+    }
+    #if the user selects day and hour data then it display x and y depending on the table value
+    else if (input$data_select == "df_day_hour") {
+      ggplot(selected_data(), aes(x = Hour, y = Total, fill = Day)) +
+        geom_bar(stat = "identity") +
+        labs(title = "Interactive Bar Chart", x = "Hour", y = "Total") +
+        theme_minimal()
+    }
+    #if the user selects day of week and month data then it display x and y depending on the table value
+    else if (input$data_select == "df_dayofweek_month") {
+      ggplot(selected_data(), aes(x = dayofweek, y = Total, fill = Month)) +
+        geom_bar(stat = "identity") +
+        labs(title = "Interactive Bar Chart", x = "dayofweek", y = "Total") +
+        theme_minimal()
+    }
+    #if the user selects base and day data then it display x and y depending on the table value
+    else if (input$data_select == "df_hour_month") {
+      ggplot(selected_data(), aes(x = Hour, y = Total, fill = Month)) +
+        geom_bar(stat = "identity") +
+        labs(title = "Interactive Bar Chart", x = "Hour", y = "Total") +
+        theme_minimal()
+    }
+    #if the user selects month and day data then it display x and y depending on the table value
+    else if (input$data_select == "df_month_day") {
+      ggplot(selected_data(), aes(x = Month, y = Total, fill = Day)) +
+        geom_bar(stat = "identity") +
+        labs(title = "Interactive Bar Chart", x = "Month", y = "Total") +
+        theme_minimal()
+    }
+    #if the user selects month then it display x and y depending on the table value
+    else if (input$data_select == "df_month") {
+      ggplot(selected_data(), aes(x = Month, y = Total)) +
+        geom_bar(stat = "identity", fill= "red") +
+        labs(title = "Interactive Bar Chart", x = "Month", y = "Total") +
+        theme_minimal()
+    }
+    #if the user selects hour then it display x and y depending on the table value
+    else if (input$data_select == "df_hour") {
+      ggplot(selected_data(), aes(x = Hour, y = Total)) +
+        geom_bar(stat = "identity", fill="forestgreen") +
+        labs(title = "Interactive Bar Chart", x = "Hour", y = "Total") +
+        theme_minimal()
+    }
+    #Prediction to know which month has the highest number of people use uber after or at 6 pm.
+    else if (input$data_select == "prediction") {
+      ggplot(df_hour_month, aes(x = Month, y = Total, color = factor(Hour > 18))) +
+        geom_point(size = 4) +
+        scale_color_manual(values = c("black", "red"), guide = FALSE) +
+        labs(color = "Hour > 18")
+  }
+})
 }
-
 # Run the application 
 shinyApp(ui = ui, server = server)
